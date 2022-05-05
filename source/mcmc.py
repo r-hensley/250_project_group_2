98af120d64457663a1e5941a3893108b5373f4f1
Gabe Lynch <fireballs619@gmail.com>
2022-05-04 21:30:31 -0700

    Barely functioning but it doesn't crash

--- source/mcmc.py
+++ source/mcmc.py
@@ -1,25 +1,26 @@
 import os
-
+from turtle import forward
 import numpy as np
 from typing import Optional, List, Tuple
-
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 from CosmoModel import CosmoModel
 
 
 class MCMC:
     def __init__(self,
-                 initial_state: Tuple[float, float, float],
+                 initial_state,
                  data_file: str,
-                 systematics_file: Optional[str],
-                 g_sigma=(0.5, 0.5, 10)) -> None:
+                 systematics_file=None,
+                 g_cov=np.diag([0.01, 0.01, 1])) -> None: 
 
-        self._chain = np.array([initial_state] + [(0., 0., 0.)] * 999)
+        self._chain = np.array(initial_state)
         self._initial_state = initial_state  # (Omega_m, Omega_L, H0)
         self._current_state = initial_state
         self._current_step = 0
-        self._generating_sigma = g_sigma
+        self._generating_cov = g_cov
+        self._generating_fisher = np.linalg.pinv(g_cov)
+        self._generating_det = np.linalg.det(g_cov)
 
         self._data_file = data_file
 
@@ -36,16 +37,9 @@ class MCMC:
 
         self._cov = self.construct_covariance()  # covariance of data
         self._fisher = np.linalg.pinv(self._cov)
+
+        self._current_log_likelihood = self.log_likelihood(initial_state)
         # Fisher matrix is inverse of covariance matrix. Just inverting ahead of time.
-
-        self._parameter_space = [np.around(np.linspace(0.1, 1, 10), decimals=1),  # Omega_m
-                                 np.around(np.linspace(0.1, 1, 10), decimals=1),  # Omega_L
-                                 np.around(np.linspace(41, 70, 30), decimals=0),  # H0
-                                 ]
-
-        # A dictionary linking positions in parameter space to likelihoods
-        # self._likelihood_lookup: Dict[Tuple[float, float, float]: float] = self.generate_likelihood_arrays()
-        self.likelihoods = np.array([self.log_likelihood(initial_state)] + [0]*999)
 
     def construct_covariance(self) -> np.ndarray:
         cov = np.diag(self._dmb)
@@ -56,7 +50,7 @@ class MCMC:
         return cov
 
     # computes log_likelihood up to a constant (i.e. unnormalized)
-    def log_likelihood(self, params: Tuple[float, float, float]) -> float:
+    def log_likelihood(self, params) -> float:
         """
         Takes in a vector of the parameters Omega_m, Omega_L, and H0, then creates a cosmological model
         off them and calculates the difference between
@@ -101,101 +95,67 @@ class MCMC:
     #
     #     return likelihood_lookup
 
-    def generator(self, position_in: Tuple[float, float, float]) -> Tuple[float, float, float]:
+    def generator(self):
         """
         Generates a new candidate position for the chain
         :param position_in: Starting position list with [Omega_m, Omega_L, H0]
         :return: Candidate next position
         """
-        candidate: List[float] = [0., 0., 0.]
-        for i in range(3):
-            candidate[i]: float = np.random.normal(
-                loc=position_in[i],  # sample from gaussian centered around current pos.
-                scale=self._generating_sigma[i])  # std. dev of distribution
-            if i == 2:
-                candidate[i] = round(candidate[i], 0)  # H0
-            else:
-                candidate[i] = round(candidate[i], 1)  # Omega_m, Omega_L
-        return candidate[0], candidate[1], candidate[2]
+        return np.random.multivariate_normal(mean=self._current_state, cov=self._generating_cov)
+
+    # equivalent to g(x,x')
+    def move_probability(self, current_state, new_state):
+        diff_vec = np.array(new_state) - np.array(current_state)
+        norm = 1/(np.sqrt((2*np.pi)**3)*np.sqrt(self._generating_det))
+        exponent = -0.5*np.einsum("i, ij, j", diff_vec.T, self._generating_fisher, diff_vec)
+        return norm*np.exp(exponent)
+
 
     def generate_acceptance_prob(self,
-                                 last_pos: Tuple[float, float, float],
-                                 candidate_pos: Tuple[float, float, float]) -> float:
+                                 current_state,
+                                 candidate_state) -> float:
         """
-        Generates acceptance probability according to Metropolis-Hastings Algorithm
+        Generates acceptance probability according to Metropolis-Hastings Algorithm.
+        For right now, no priors are included.
         :param last_pos: Starting position
         :param candidate_pos: Potential new position
         :return: A probability for accepting the new position
         """
-        # Posterior probability of last position P(x_t)
-        last_pos_prob = self.likelihoods[self._current_step-1]
-        candidate_pos_prob = self.log_likelihood(candidate_pos)
+        new_log_likelihood = self.log_likelihood(candidate_state)
+        back_prob = self.move_probability(candidate_state, current_state)
+        forward_prob = self.move_probability(current_state, candidate_state)
 
-        # Posterior probability of new position P(x')
-        # if candidate_pos in self._likelihood_lookup:
-        #     candidate_pos_prob = self._likelihood_lookup[candidate_pos]
-        # else:
-        #     candidate_pos_prob = 0.
+        diff = new_log_likelihood + back_prob - self._current_log_likelihood - forward_prob
 
-        # The probabilities to move back and forth from the new and old positions
-        # g(x'|x_t)
-        move_forward_prob = evaluate_gaussian(candidate_pos, last_pos, self._generating_sigma)
-        # g(x_t|x')
-        move_backward_prob = evaluate_gaussian(last_pos, candidate_pos, self._generating_sigma)
-
-        acceptance_prob: float = min(1., candidate_pos_prob * move_backward_prob / last_pos_prob / move_forward_prob)
-        return acceptance_prob
+        return np.min([1., np.exp(diff)]), new_log_likelihood
 
     def propagate_chain(self) -> None:
         """
-        Advances the Markov chain by one time step
+        Advances the Markov chain by one step
         :return: The propagated Markov chain
         """
-        self._current_step += 1
-        last_pos = tuple(self._chain[self._current_step - 1])  # starting position x_t
-        candidate_pos = self.generator(last_pos)  # new position x'
-        # for i, j in enumerate(candidate_pos):
-        #     if j not in self._parameter_space[i]:
-        #         new_parameter = find_closest(self._parameter_space[i], candidate_pos[i])
-        #         print(f"Relocated point {candidate_pos[i]} in array {i} to {new_parameter}")
-        #         candidate_pos[i] = new_parameter
-        # candidate_pos = (candidate_pos[0], candidate_pos[1], candidate_pos[2])
-
-        acceptance_prob = self.generate_acceptance_prob(last_pos, candidate_pos)
+        candidate_state = self.generator()  # new position x'
+        acceptance_prob, new_log_likelihood = self.generate_acceptance_prob(self._current_state, candidate_state)
 
         random_number = np.random.uniform(0, 1)
 
         if random_number <= acceptance_prob:
-            self._chain[self._current_step] = candidate_pos
+            self._chain = np.vstack([self._chain, candidate_state])
+            self._current_state = candidate_state
+            self._current_log_likelihood = new_log_likelihood
         else:
-            self._chain[self._current_step] = last_pos
+            self._chain = np.vstack([self._chain, self._current_state])
 
-    def make_chain(self):
-        for _ in range(len(self._chain)-1):
+        self._current_step += 1
+
+    def make_chain(self, n):
+        for _ in range(n):
             self.propagate_chain()
             print(f"Step #{self._current_step}")
 
     @property
     def chain(self):
         return self._chain
-
-
-def evaluate_gaussian(pos: Tuple[float, float, float],
-                      loc: Tuple[float, float, float],
-                      scale: Tuple[float, float, float]) -> float:
-    """
-    Evaluates the probability density for a Gaussian
-    :param pos: Position on the Gaussian
-    :param loc: Mean value of Gaussian
-    :param scale: Width of gaussian
-    :return: Probability density
-    """
-    # normalization is wrong but for now it's close enough
-    gaussian = np.sqrt((2 * np.pi) ** 3 * np.prod(scale))  # normalization
-    for i in range(3):
-        gaussian *= np.exp(-(pos[i] - loc[i]) ** 2 / (2 * scale[i] ** 2))
-    return gaussian
-
 
 # def find_closest(array: np.ndarray, value: Union[int, float]) -> float:
 #     """
@@ -218,27 +178,22 @@ def main():
 
     # The for loop is to allow for the option of plotting multiple chains on the same chart
     # (It just kinda looks cool)
-    for _ in range(1):
-        print(f"Starting Markov Chain #{_ + 1}")
-        start = (round(np.random.uniform(0.1, 2), 1),  # Omega_m
-                 round(np.random.uniform(0.1, 2), 1),  # Omega_L
-                 round(np.random.uniform(1, 100), 0))  # H0
-        markov_chain = MCMC(initial_state=start,
-                            data_file=binned_data_file,
-                            systematics_file=binned_sys_file,
-                            g_sigma=(0.1, 0.1, 10))
+    print(f"Starting Markov Chain")
+    start = np.array([0.5, 0.5, 50])
+    markov_chain = MCMC(initial_state=start,
+                        data_file=binned_data_file,
+                        systematics_file=binned_sys_file)
 
-        markov_chain.make_chain()
+    markov_chain.make_chain(5000)
 
-        figs, axs = plt.subplots(1, 3)
+    print(markov_chain._chain)
+
+    figs, axs = plt.subplots(1, 3)
         # plt.rcParams['figure.figsize'] = [20, 7]
-        for i in range(3):
-            axs[i].plot(markov_chain.chain[:, i])
+    for i in range(3):
+        axs[i].plot(markov_chain.chain[:, i])
 
-        plt.show()
-
+    plt.show()
 
 if __name__ == "__main__":
     main()
-
-print("Done")
