3c02feb390ed98e8f424745c2411093a3d6b557f
ryry013 <30587035+ryry013@users.noreply.github.com>
2022-05-06 12:21:04 -0700

    Remove likelihood from state class

--- source/mcmc_state.py
+++ source/mcmc_state.py
@@ -1,29 +1,37 @@
 from dataclasses import dataclass
-from typing import Union, Tuple
+from typing import Union, Tuple, Any
 
 import numpy as np
 
 from mcmc import MCMC
 
 
+def is_float_or_int(number: Any) -> bool:
+    """
+    Checks whether a number is a float or an instance, including instances of numpy types such as float64
+    :param number: Number to check
+    :return: Boolean
+    """
+    return isinstance(number, (float, int, np.float, np.integer))
+
+
 @dataclass
 class State:
     def __init__(self,
-                 mcmc: MCMC,
-                 *args: Tuple[Union[list, tuple, np.ndarray, float, ...]]) -> None:
+                 *args  # : Tuple[Union[list, tuple, np.ndarray, float]]) -> None:
+                 ) -> None:
         """
         A class containing information about a state in the MCMC algorithm.
         :param mcmc: The mcmc class instance to use the log_likelihood() function
         :param args: Takes either a list/array/tuple containing four parameters,
         or the four parameters as separate arguments
         """
-        self._mcmc = mcmc
         if len(args) == 1:
             if type(args[0]) not in [list, tuple, np.ndarray]:
                 raise TypeError(f"The single argument passed must be of type list, tuple, or np.ndarray. "
                                 f"(type {type(args[0])} passed)")
 
-            assert not [True for arg in args[0] if type(arg) != float]  # All elements of arg must be floats
+            assert not [True for arg in args[0] if not is_float_or_int(arg)], "Arguments must be ints or floats"
 
             self._Omega_m = args[0][0]
             self._Omega_L = args[0][1]
@@ -31,7 +39,7 @@ class State:
             self._M = args[0][3]
         
         elif len(args) == 4:
-            assert not [True for arg in args if type(arg) != float]  # All elements of arg must be floats
+            assert not [True for arg in args if not is_float_or_int(arg)], "Arguments must be ints or floats"
 
             self._Omega_m = args[0]
             self._Omega_L = args[1]
@@ -62,11 +70,3 @@ class State:
         :return: An array containing the parameters [Omega_m, Omega_L, H0, M]
         """
         return np.ndarray([self._Omega_m, self._Omega_L, self._H0, self._M])
-        
-    @property
-    def likelihood(self) -> float:
-        """
-        Calculates and returns the log likelihood based on the log_likelihood() function in the MCMC class.
-        :return: Log likelihood of the current state
-        """
-        return self._mcmc.log_likelihood(self)
