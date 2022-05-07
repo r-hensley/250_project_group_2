d679777df853c1d5d5073fff1bd2e3f5ff7d92d5
Ryry013 <30587035+ryry013@users.noreply.github.com>
2022-05-06 21:12:07 -0700

    Continued work on getting State class working

--- source/mcmc_state.py
+++ source/mcmc_state.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-from typing import Union, Tuple, Any
+from typing import Any
 
 import numpy as np
 
@@ -15,7 +14,7 @@ def is_float_or_int(number: Any) -> bool:
 # Reference for subclassing np.ndarray:
 # https://numpy.org/doc/stable/user/basics.subclassing.html#simple-example-adding-an-extra-attribute-to-ndarray
 
-@dataclass
+
 class State(np.ndarray):
     """
     A class containing information about a state in the MCMC algorithm.
@@ -26,115 +25,101 @@ class State(np.ndarray):
         The class takes input as a single array-like object or a series of four arguments
         :param args: Either a single array or exactly four float arguments
         """
-        obj = super().__new__(cls, shape=(4,), dtype=float, buffer=None, offset=0, strides=None, order=None)
-        print(f"in __new__ with cls {cls}")
-        print(f"args in __new__: {args}")
-
         if len(args) == 1:
             if type(args[0]) not in [list, tuple, np.ndarray]:
                 raise TypeError(f"The single argument passed must be of type list, tuple, or np.ndarray. "
                                 f"(type {type(args[0])} passed)")
 
             assert not [True for arg in args[0] if not is_float_or_int(arg)], "Arguments must be ints or floats"
-
-            obj._Omega_m = float(args[0][0])
-            obj._Omega_L = float(args[0][1])
-            obj._H0 = float(args[0][2])
-            obj._M = float(args[0][3])
+            obj = np.asarray(args[0]).view(cls)  # normal numpy array reclassed as State type
 
         elif len(args) == 4:
             assert not [True for arg in args if not is_float_or_int(arg)], "Arguments must be ints or floats"
-
-            obj._Omega_m = float(args[0])
-            obj._Omega_L = float(args[1])
-            obj._H0 = float(args[2])
-            obj._M = float(args[3])
+            obj = np.asarray(args).view(cls)  # normal numpy array reclasssed as State type
 
         else:
             raise TypeError(f"Please pass either one list/tuple/array argument or four separate arguments. "
                             f"({len(args)} arguments passed)")
 
-        print("end of __new__", obj, obj._Omega_m)
         return obj
 
     def __array_finalize__(self, obj):
         if obj is None:
             return
-        self._Omega_m = getattr(obj, '_Omega_m', None)
-        self._Omega_L = getattr(obj, '_Omega_L', None)
-        self._H0 = getattr(obj, '_H0', None)
-        self._M = getattr(obj, '_M', None)
-        print(f"obj in __new__: {obj}")
-        print("end of __array_finalize__", self._Omega_m)
-
-    # def __init__(self, *args):
-    #     super().__init__(self)
 
     @property
     def tuple(self) -> tuple:
         """
         :return: A tuple containing the parameters (Omega_m, Omega_L, H0, M)
         """
-        return self._Omega_m, self._Omega_L, self._H0, self._M
+        return self[0], self[1], self[2], self[3]
 
     @property
     def list(self) -> list:
         """
         :return: A list containing the parameters [Omega_m, Omega_L, H0, M]
         """
-        return [self._Omega_m, self._Omega_L, self._H0, self._M]
+        return [self[0], self[1], self[2], self[3]]
 
     @property
     def array(self) -> np.ndarray:
         """
         :return: An array containing the parameters [Omega_m, Omega_L, H0, M]
         """
-        return np.array([self._Omega_m, self._Omega_L, self._H0, self._M])
-
-    def __iter__(self) -> np.ndarray:
-        """
-        Makes this class iterable to be accepted as an element of a numpy array
-        :return: Numpy ndarray
-        """
-        return self.array
+        return np.array([self[0], self[1], self[2], self[3]])
 
     def __str__(self) -> str:
         """
         Changes how this class is represented when printed as a string
         :return: String representation of object
         """
-        return f"(Omega_m: {self._Omega_m}, " \
-               f"Omega_L: {self._Omega_L}, " \
-               f"H0: {self._H0}, " \
-               f"M: {self._M})"
+        return f"(Omega_m: {self[0]}, " \
+               f"Omega_L: {self[1]}, " \
+               f"H0: {self[2]}, " \
+               f"M: {self[3]})"
 
     def __repr__(self) -> str:
         """
         Changes how the class is represented when printed in development settings, or as a list element
         :return: Representation of string
         """
-        params = [("Omega_m", round(self._Omega_m, 2)),
-                  ("Omega_L", round(self._Omega_L, 2)),
-                  ("H0", round(self._H0, 2)),
-                  ("M", round(self._M, 2))]
+        params = [("Omega_m", round(self[0], 2)),
+                  ("Omega_L", round(self[1], 2)),
+                  ("H0", round(self[2], 2)),
+                  ("M", round(self[3], 2))]
         inner = ', '.join([f"{i[0]}: {i[1]}" for i in params])
         return f"<{self.__class__.__name__} {inner}>"
 
-    def __getitem__(self, item: int) -> float:
-        """
-        Makes the class subscriptable
-        :param item: Integer representing one of the elements of the class
-        :return: Float representing one of the parameters
-        """
-        return self.list[item]
+    @property
+    def Omega_m(self):
+        return self[0]
+
+    @property
+    def Omega_L(self):
+        return self[1]
+
+    @property
+    def H0(self):
+        return self[2]
+
+    @property
+    def M(self):
+        return self[3]
 
 
-s1 = State(1,2,3,4)
-s2 = State(5,6,7,8)
-
-print(s1.tuple)
-print(s1[0])
-
-print(s1 + s2)
-print(s1 * 4)
-
+# s1 = State(1, 2, 3, 4)
+# s2 = State(5, 6, 7, 8)
+#
+# print('s1 tuple', s1.tuple)
+# print('s1[0]', s1[0])
+#
+# print('str s1', s1)
+# print('repr s1', [s1])
+#
+# print('s1 values', s1.Omega_m, s1.Omega_L, s1.H0, s1.M)
+# print('s2 values', s2.Omega_m, s2.Omega_L, s2.H0, s2.M)
+#
+# s3 = s1 + s2
+# print('s3 0', s3[0])
+# print('add two lists', s1 + s2)
+# print('s1*4', s1 * 4)
